9f20d5bde44894bbf4434b9f963f32ec53fa2ed6
Ping Chen <remotenonsense@gmail.com>
2021-06-16 13:00:10 +0300

    Bug 1716704 - Fix edit contact for LDAP card. r=mkmelin a=rjl

--- mail/components/addrbook/content/abCard.js
+++ mail/components/addrbook/content/abCard.js
@@ -289,7 +289,7 @@ function OnLoadEditCard() {
 
         // And the phonetic fields
         document.getElementById(kPhoneticFields[0]).readOnly = true;
-        document.getElementById(kPhoneticFields[3]).readOnly = true;
+        document.getElementById(kPhoneticFields[2]).readOnly = true;
 
         // Also disable the mail format popup and allow remote content items.
         document.getElementById("PreferMailFormatPopup").disabled = true;
