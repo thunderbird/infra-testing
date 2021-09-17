d8a326da3befd00a3857633d4286ca369a001fb0
Magnus Melin <mkmelin+mozilla@iki.fi>
2021-09-17 13:10:38 +0300

    Bug 119384 - [s/mime] warn when using BCC with encryption. r=lasana a=wsmwk
    Differential Revision: https://phabricator.services.mozilla.com/D125059

--- mail/components/compose/content/MsgComposeCommands.js
+++ mail/components/compose/content/MsgComposeCommands.js
@@ -5413,11 +5413,7 @@ async function checkEncryptedBccRecipients() {
     pill => pill.emailAddress == gCurrentIdentity.email
   );
 
-  if (
-    !(gSendEncrypted && gSelectedTechnologyIsPGP) ||
-    !bccRecipients.length ||
-    bccIsSender
-  ) {
+  if (!gSendEncrypted || !bccRecipients.length || bccIsSender) {
     if (notification) {
       gComposeNotification.removeNotification(notification);
     }
