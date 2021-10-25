7669de0a54b6fb4361b614ff59d3aa18db201af7
Alessandro Castellani <alessandro@thunderbird.net>
2021-10-25 13:30:32 +0300

    Bug 1737386 - Fix attachments onDrop event not firing in macOS and Windows. r=Paenglab a=wsmwk
    Differential Revision: https://phabricator.services.mozilla.com/D129357

--- mail/components/compose/content/MsgComposeCommands.js
+++ mail/components/compose/content/MsgComposeCommands.js
@@ -8531,6 +8531,9 @@ var envelopeDragObserver = {
     // No need to check for the same dragged files if the previous dragging
     // action didn't end.
     if (gIsDraggingAttachments) {
+      // Prevent the default action of the event otherwise the onDrop event
+      // won't be triggered.
+      event.preventDefault();
       this.detectHoveredOverlay(event.target.id);
       return;
     }
