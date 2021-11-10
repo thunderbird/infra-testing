cf86b693d6031249e94f872c124ac780058efb5b
Magnus Melin <mkmelin+mozilla@iki.fi>
2021-11-10 15:35:35 +0200

    Bug 1740213 - when opening pdf attachments in the 3pane, make sure to put the 3pane in focus. r=henry a=wsmwk
    Differential Revision: https://phabricator.services.mozilla.com/D130867

--- mail/base/content/msgHdrView.js
+++ mail/base/content/msgHdrView.js
@@ -1971,6 +1971,7 @@ AttachmentInfo.prototype = {
               background: false,
               linkHandler: "single-page",
             });
+            tabmail.ownerDocument.defaultView.focus();
             return;
           }
           // If no tabmail, open PDF same as other attachments.
