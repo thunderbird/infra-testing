4322ec6e9e39f1786d1ecb851c674b5251a1ec3d
Geoff Lankow <geoff@darktrojan.net>
2022-01-13 12:09:34 +1300

    Bug 1749757 - Provide a browsingContext id to app launcher dialog when opening attachments. r=aleca a=wsmwk
    Differential Revision: https://phabricator.services.mozilla.com/D135754

--- mail/base/content/msgHdrView.js
+++ mail/base/content/msgHdrView.js
@@ -2241,7 +2241,7 @@ AttachmentInfo.prototype = {
           targetFileIsExecutable: null,
           timeDownloadStarted: null,
           contentLength: this.size,
-          browsingContextId: null,
+          browsingContextId: getMessagePaneBrowser().browsingContext.id,
         },
         window,
         null
