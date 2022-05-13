c9eb555cbeb34cc3457ba487a212306d4d89374b
Rob Lemley <rob@thunderbird.net>
2022-05-13 12:59:54 +0300

    Bug 1768936 - Port bug 1712104: Use PrintUtils.handleStaticCloneCreatedForPrint. r=mkmelin a=wsmwk
    Port of D135451.
    This keeps Thunderbird printing aligned with Firefox and enables the pref
    `print.prefer_system_dialog` (from bug 1712104) to bypass print preview.
    
    Differential Revision: https://phabricator.services.mozilla.com/D146116

--- mail/base/content/mailWindow.js
+++ mail/base/content/mailWindow.js
@@ -918,9 +918,9 @@ nsBrowserAccess.prototype = {
     aSkipLoad
   ) {
     if (aWhere == Ci.nsIBrowserDOMWindow.OPEN_PRINT_BROWSER) {
-      let browser = PrintUtils.startPrintWindow(aOpenWindowInfo.parent, {
-        openWindowInfo: aOpenWindowInfo,
-      });
+      let browser = PrintUtils.handleStaticCloneCreatedForPrint(
+        aOpenWindowInfo
+      );
       return browser ? browser.browsingContext : null;
     }
 
@@ -993,9 +993,9 @@ nsBrowserAccess.prototype = {
     aSkipLoad
   ) {
     if (aWhere == Ci.nsIBrowserDOMWindow.OPEN_PRINT_BROWSER) {
-      return PrintUtils.startPrintWindow(aParams.openWindowInfo.parent, {
-        openWindowInfo: aParams.openWindowInfo,
-      });
+      return PrintUtils.handleStaticCloneCreatedForPrint(
+        aParams.openWindowInfo
+      );
     }
 
     if (aWhere != Ci.nsIBrowserDOMWindow.OPEN_NEWTAB) {
