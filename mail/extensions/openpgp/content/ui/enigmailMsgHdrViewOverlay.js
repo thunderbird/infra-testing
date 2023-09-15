66e16c39a57b0fe76c2893e2e7785e1b5d1fe952
welpy-cw <h.w.forms@arcor.de>
2023-09-15 23:03:57 +0000

    Bug 1831764 - Restore invalidating the thread tree when setting the subject in PGP code. r=mkmelin a=wsmwk
    Based on https://github.com/Betterbird/thunderbird-patches/blob/main/115/bugs/1843833-fix-subject-pgp.patch
    
    Differential Revision: https://phabricator.services.mozilla.com/D187903

--- mail/extensions/openpgp/content/ui/enigmailMsgHdrViewOverlay.js
+++ mail/extensions/openpgp/content/ui/enigmailMsgHdrViewOverlay.js
@@ -761,13 +761,18 @@ Enigmail.hdrView = {
 
     // Update the message.
     gMessage.subject = newSubject;
+    let oldFlags = gMessage.flags;
     if (hadRe) {
       gMessage.flags |= Ci.nsMsgMessageFlags.HasRe;
       newSubject = "Re: " + newSubject;
     }
-    document.title = newSubject;
-    currentHeaderData.subject.headerValue = newSubject;
-    document.getElementById("expandedsubjectBox").headerValue = newSubject;
+    // This even works if the flags haven't changed. Causes repaint in all thread trees.
+    gMessage.folder?.msgDatabase.notifyHdrChangeAll(
+      gMessage,
+      oldFlags,
+      gMessage.flags,
+      {}
+    );
   },
 
   updateHdrBox(header, value) {
