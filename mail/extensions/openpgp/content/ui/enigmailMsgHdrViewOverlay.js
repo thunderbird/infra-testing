d3b3d8acfdbbcff9e7ef851076b91fba48b6c760
Daniel Darnell <daniel@thunderbird.net>
2023-09-25 16:49:20 -0700

    Backed out changeset 858f41ca7400 (bug 1831764) rs=backout a=dandarnell

--- mail/extensions/openpgp/content/ui/enigmailMsgHdrViewOverlay.js
+++ mail/extensions/openpgp/content/ui/enigmailMsgHdrViewOverlay.js
@@ -761,7 +761,6 @@ Enigmail.hdrView = {
 
     // Update the message.
     gMessage.subject = newSubject;
-    let oldFlags = gMessage.flags;
     if (hadRe) {
       gMessage.flags |= Ci.nsMsgMessageFlags.HasRe;
       newSubject = "Re: " + newSubject;
@@ -769,13 +768,6 @@ Enigmail.hdrView = {
     document.title = newSubject;
     currentHeaderData.subject.headerValue = newSubject;
     document.getElementById("expandedsubjectBox").headerValue = newSubject;
-    // This even works if the flags haven't changed. Causes repaint in all thread trees.
-    gMessage.folder.msgDatabase.notifyHdrChangeAll(
-      gMessage,
-      oldFlags,
-      gMessage.flags,
-      {}
-    );
   },
 
   updateHdrBox(header, value) {
