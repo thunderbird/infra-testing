f3bda0a37c750afc44f21aed46a012fa7cf41dd5
Magnus Melin <mkmelin+mozilla@iki.fi>
2023-10-06 16:21:34 +0000

    Bug 1857196 - CalIcsParser error with empty ATTENDEE:. r=leftmostcat a=wsmwk
    Differential Revision: https://phabricator.services.mozilla.com/D190271

--- calendar/test/unit/test_bug653924.js
+++ calendar/test/unit/test_bug653924.js
@@ -10,8 +10,8 @@ XPCOMUtils.defineLazyModuleGetters(this, {
 });
 
 function run_test() {
-  let evt = new CalEvent();
-  let rel = new CalRelation();
+  const evt = new CalEvent();
+  const rel = new CalRelation("RELATED-TO:2424d594-0453-49a1-b842-6faee483ca79");
   evt.addRelation(rel);
 
   equal(1, evt.icalString.match(/RELATED-TO/g).length);
