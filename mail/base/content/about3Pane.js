c6aaa0c7c82aab0992ab5b3c27b04471250899cd
welpy-cw <h.w.forms@arcor.de>
2023-10-26 17:52:10 +0000

    Bug 1839035 - Completely refresh dummy rows in Grouped By view. r=aleca a=wsmwk
    Inspired by https://github.com/Betterbird/thunderbird-patches/blob/main/115/bugs/1839035-clean-dummy-row.patch
    
    Differential Revision: https://phabricator.services.mozilla.com/D191558

--- mail/base/content/about3Pane.js
+++ mail/base/content/about3Pane.js
@@ -6090,17 +6090,7 @@ customElements.whenDefined("tree-view-table-row").then(() => {
       let ariaLabelPromises = [];
 
       const propertiesSet = new Set(properties.value.split(" "));
-
-      if (propertiesSet.has("dummy")) {
-        const cell = this.querySelector(".subjectcol-column");
-        const textIndex = textColumns.indexOf("subjectCol");
-        const label = cellTexts[textIndex];
-        const span = cell.querySelector(".subject-line span");
-        cell.title = span.textContent = label;
-        this.setAttribute("aria-label", label);
-        this.dataset.properties = "dummy";
-        return;
-      }
+      const isDummyRow = propertiesSet.has("dummy");
 
       this.dataset.properties = properties.value.trim();
 
@@ -6122,7 +6112,7 @@ customElements.whenDefined("tree-view-table-row").then(() => {
 
           let imageFluentID = this.#getMessageIndicatorString(propertiesSet);
           const image = div.querySelector("img");
-          if (imageFluentID) {
+          if (imageFluentID && !isDummyRow) {
             document.l10n.setAttributes(image, imageFluentID);
           } else {
             image.removeAttribute("data-l10n-id");
@@ -6210,6 +6200,10 @@ customElements.whenDefined("tree-view-table-row").then(() => {
         }
 
         if (textIndex >= 0) {
+          if (isDummyRow) {
+            cell.textContent = "";
+            continue;
+          }
           cell.textContent = cellTexts[textIndex];
           ariaLabelPromises.push(cellTexts[textIndex]);
         }
