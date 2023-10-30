fa1aab9df21c40332fdf3524d648efd4f7992043
welpy-cw <h.w.forms@arcor.de>
2023-10-30 04:13:22 +0000

    Bug 1827042 - Follow-up fix for scroll position in quick search. r=darktrojan a=wsmwk
    Differential Revision: https://phabricator.services.mozilla.com/D192114

--- mail/base/content/about3Pane.js
+++ mail/base/content/about3Pane.js
@@ -5024,7 +5024,7 @@ var threadPane = {
    * selected.
    */
   scrollToLatestRowIfNoSelection() {
-    if (gDBView.selection.count > 0 || gDBView.rowCount <= 0) {
+    if (!gDBView || gDBView.selection.count > 0 || gDBView.rowCount <= 0) {
       return;
     }
     if (
