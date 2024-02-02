03df2e31b8ae9777011fc3ed4bf415b01499cfe9
Geoff Lankow <geoff@darktrojan.net>
2024-02-02 17:42:22 +1300

    Bug 1877884 - Stop browser_interactionTelemetry.js timing out waiting for focus in another place. r=mkmelin a=wsmwk We don't need this bit.
    Differential Revision: https://phabricator.services.mozilla.com/D200428

--- mail/base/test/browser/head.js
+++ mail/base/test/browser/head.js
@@ -6,17 +6,7 @@ var { MailServices } = ChromeUtils.import(
   "resource:///modules/MailServices.jsm"
 );
 
-async function focusWindow(win) {
-  win.focus();
-  await TestUtils.waitForCondition(
-    () => Services.focus.focusedWindow?.browsingContext.topChromeWindow == win,
-    "waiting for window to be focused"
-  );
-}
-
 async function clickExtensionButton(win, buttonId) {
-  await focusWindow(win.top);
-
   buttonId = CSS.escape(buttonId);
   let actionButton = await TestUtils.waitForCondition(
     () =>
