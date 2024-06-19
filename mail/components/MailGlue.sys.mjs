7b00c28ab4bc7f1de7037778e67dc958726a1be2
Magnus Melin <mkmelin+mozilla@iki.fi>
2024-06-19 10:19:12 +0300

    Bug 1899602 - migrate tb.account.count to glean. r=aleca
    Differential Revision: https://phabricator.services.mozilla.com/D213105

--- mail/components/MailGlue.sys.mjs
+++ mail/components/MailGlue.sys.mjs
@@ -1037,7 +1037,7 @@ function reportAccountTypes() {
   }
 
   for (const [type, count] of Object.entries(report)) {
-    Services.telemetry.keyedScalarSet("tb.account.count", type, count);
+    Glean.tb.accountCount[type].set(count);
   }
 
   for (const [provider, count] of Object.entries(providerReport)) {
