67a5a18e85f73ac96e8bca3c57893b89ed09db91
Magnus Melin <mkmelin+mozilla@iki.fi>
2024-06-19 10:19:34 +0300

    Bug 1899602 - migrate tb.account.successful_email_account_setup to glean. r=aleca
    Apparently this was not tested before.
    
    Differential Revision: https://phabricator.services.mozilla.com/D213106

--- mail/components/accountcreation/content/accountSetup.js
+++ mail/components/accountcreation/content/accountSetup.js
@@ -2432,12 +2432,7 @@ var gAccountSetup = {
             successfulConfig.outgoing.oauthSettings;
         }
         self.finish(configFilledIn);
-
-        Services.telemetry.keyedScalarAdd(
-          "tb.account.successful_email_account_setup",
-          telemetryKey,
-          1
-        );
+        Glean.tb.successfulEmailAccountSetup[telemetryKey].add(1);
       })
       .catch(e => {
         // failed
