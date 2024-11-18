044989a694b686faf9d978717c9b67bf7fffda90
Alessandro Castellani <alessandro@thunderbird.net>
2024-11-18 17:22:51 -0500

    Bug 1925383 - [ESR115] Show a 20 years donation appeal inside a Thunderbird tab. r=coreycb,sancus a=coreycb

--- mail/base/content/messenger.js
+++ mail/base/content/messenger.js
@@ -474,10 +474,28 @@ var gMailInit = {
    */
   showEOYDonationAppeal() {
     let url = Services.prefs.getStringPref("app.donation.eoy.url");
-    let protocolSvc = Cc[
-      "@mozilla.org/uriloader/external-protocol-service;1"
-    ].getService(Ci.nsIExternalProtocolService);
-    protocolSvc.loadURI(Services.io.newURI(url));
+    let tabmail = document.getElementById("tabmail");
+
+    if (!tabmail) {
+      tabmail = Services.wm
+        .getMostRecentWindow("mail:3pane")
+        ?.document.getElementById("tabmail");
+    }
+
+    // Fall back to opening a browser window if we don't have a tabmail.
+    if (!tabmail) {
+      const protocolSvc = Cc[
+        "@mozilla.org/uriloader/external-protocol-service;1"
+      ].getService(Ci.nsIExternalProtocolService);
+      protocolSvc.loadURI(Services.io.newURI(url));
+    } else {
+      tabmail.openTab("contentTab", {
+        url,
+        background: false,
+        linkHandler: "single-page",
+      });
+      tabmail.ownerGlobal.focus();
+    }
 
     let currentEOY = Services.prefs.getIntPref("app.donation.eoy.version", 1);
     Services.prefs.setIntPref("app.donation.eoy.version.viewed", currentEOY);
