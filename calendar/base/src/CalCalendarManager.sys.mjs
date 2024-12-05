83cdb794c93200bee8a03a1c5afc31583ef7e9a2
Magnus Melin <mkmelin+mozilla@iki.fi>
2024-12-05 22:26:03 +0000

    Bug 1862400 - "Prefer client-side email scheduling" needs to be selected twice. r=leftmostcat a=coreycbryant
    There were two bugs:
     - forceEmailScheduling property not listed in the properties to copy over when switching to cached calendar
     - gCalendar is not correct after the switch
    
    Differential Revision: https://phabricator.services.mozilla.com/D231276

--- calendar/base/src/CalCalendarManager.sys.mjs
+++ calendar/base/src/CalCalendarManager.sys.mjs
@@ -747,7 +747,6 @@ calMgrCalendarObserver.prototype = {
   },
 
   changeCalendarCache(aCalendar, aName, aValue, aOldValue) {
-    const cICM = Ci.calICalendarManager;
     aOldValue = aOldValue || false;
     aValue = aValue || false;
 
@@ -773,7 +772,7 @@ calMgrCalendarObserver.prototype = {
       // it so the registerCalendar call can wrap/unwrap the
       // calCachedCalendar facade saving the user the need to
       // restart Thunderbird and making sure a new Id is used.
-      this.calMgr.removeCalendar(aCalendar, cICM.REMOVE_NO_DELETE);
+      this.calMgr.removeCalendar(aCalendar, Ci.calICalendarManager.REMOVE_NO_DELETE);
       const newCal = this.calMgr.createCalendar(aCalendar.type, aCalendar.uri);
       newCal.name = aCalendar.name;
 
@@ -783,6 +782,7 @@ calMgrCalendarObserver.prototype = {
       const propsToCopy = [
         "color",
         "disabled",
+        "forceEmailScheduling",
         "auto-enabled",
         "cache.enabled",
         "refreshInterval",
