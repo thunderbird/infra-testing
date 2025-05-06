b047b7c8d79a18a528e6cc9a8ea0119e4d375d68
welpy-cw <h.w.forms@arcor.de>
2025-05-06 12:10:45 -0400

    Bug 1960349 - Refresh standalone message windows after compacting.r=arschmitz a=coreycb
    Differential Revision: https://phabricator.services.mozilla.com/D247697

--- mail/base/content/mailCommon.js
+++ mail/base/content/mailCommon.js
@@ -1037,6 +1037,11 @@ var dbViewWrapperListener = {
     this._allMessagesLoaded = false;
 
     if (!window.threadTree || !gViewWrapper) {
+      if (location.href == "about:message" && window.msgLoading) {
+        // Apparently the view has been re-created after the underlying folder
+        // has been compacted.
+        window.ReloadMessage();
+      }
       return;
     }
 
